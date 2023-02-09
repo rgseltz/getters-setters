4103d9226a40cb64b8f903a209e32c4e49270b14
ryan <ryanjgecelter@gmail.com>
2023-02-09 12:42:35 +0200

    added dom class methods

--- app.js
+++ app.js
@@ -11,7 +11,39 @@ class Temperature {
     static fromFarenheit(value) {
         return new Temperature((value - 32) / 1.8);
     }
+    getElement(id) {
+        return document.getElementById(`${id}`);
+    }
+    displayResults(celcius, farenheit) {
+        console.log('c', celcius);
+        console.log('f', farenheit);
+        return document.getElementById('results').innerHTML = `Celcisus : ${celcius} | Farenheit : ${farenheit}`
+    }
+    handleTemperature() {
+        const celciusInput = this.getElement('celcius-temp');
+        const farenheitInput = this.getElement('farenheit-temp');
+        // const temp = celcisusInput.value ? new Temperature(celcisusInput.value) : Temperature.fromFarenheit(farenheitInput.value);
+        farenheitInput.value = this.farenheit
+        this.displayResults(celciusInput.value || this.celcius, this.farenheit);
+        celciusInput.value = '';
+        farenheitInput.value = '';
+    }
 }
 
 const tempBtn = document.querySelector('#temp-button');
-tempBtn.addEventListener('click', handleTemperature);
+
+function initApp() {
+    tempBtn.addEventListener('click', clickHandler);
+}
+
+function clickHandler(evt) {
+    evt.preventDefault();
+    const temp = !document.getElementById('celcius-temp').value ? Temperature.fromFarenheit(document.getElementById('farenheit-temp').value) : new Temperature(document.getElementById('celcius-temp').value);
+    console.log(temp);
+    temp.handleTemperature();
+    // temp.displayResults(temp.celcius, temp.farenheit);
+}
+initApp();
+
+
+
